c17bc7342e6e5d84bbd7f345ab365b22ba02781a
LLFFYY <840187818@qq.com>
2020-03-19 13:12:42 +0800

    vuex异步操作actions

--- src/js/Vuex.js
+++ src/js/Vuex.js
@@ -3,11 +3,24 @@ import Vue from 'vue'
 Vue.use(Vuex)
 const store=new Vuex.Store({
     state:{
-        count:33
+        count:33,
     },
     mutations:{
         add(state){
             state.count++
+        },
+        reduce(state){
+            state.count--
+        }
+    },
+    actions:{
+        reduce(x){
+            return new Promise((resolve,reject)=>{
+                setTimeout(()=>{
+                    x.commit('reduce')
+                    resolve()
+                },3000)
+            })
         }
     }
 })
--- src/page/homes/home.vue
+++ src/page/homes/home.vue
@@ -2,11 +2,12 @@
     <div>
         <div>data取值：{{num}}</div>
         <div>计算属性:{{kk}}</div>
-        <div @click="addButton">值加加</div>
+        <div @click="addButton" class="border">值加加</div>
+        <div class="border" v-on:click="reduceButton">值延时减少</div>
     </div>
 </template>
 <script>
-import vuexs from '@/js/Vuex.js'
+//import vuexs from '@/js/Vuex.js'
 export default {
     components:{},
     data(){
@@ -16,20 +17,34 @@ export default {
     },
     computed:{
         kk(){
-            return vuexs.state.count
+            //return vuexs.state.count
+            return this.$store.state.count
         }
     },
     created(){
-        this.num=vuexs.state.count
+       // this.num=vuexs.state.count
+            this.num=this.$store.state.count
     },
     mounted(){},
     methods:{
         addButton:function(){
-            vuexs.commit('add')
+           // vuexs.commit('add')
+           this.$store.commit('add')
+        },
+        reduceButton:function(){
+            //vuexs.comit
+            this.$store.dispatch('reduce').then(()=>{
+                alert('0')
+            })
         }
     }
 }
 </script>
 <style lang="">
-    
+    .border{
+        border:1px solid red;
+        padding:2px 1rem;
+        margin:1rem 0;
+        display: inline-block;
+    }
 </style>
